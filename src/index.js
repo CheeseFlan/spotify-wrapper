3e87f2a68c8b490250492ea293f4f5fddcad2366
John Henrique <joaoh3326@gmail.com>
2017-08-30 16:50:46 -0300

    lint

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
-import {
-  search,
-  searchArtists,
-  searchAlbums,
-  searchPlaylists,
-} from './search';
+/* global fetch */
 
-import {
-  getAlbum,
-  getAlbums,
-  getAlbumTracks,
-} from './album';
+import search from './search';
+import album from './album';
 
-module.exports = {
-  search,
-  searchArtists,
-  searchAlbums,
-  searchPlaylists,
-  getAlbum,
-  getAlbums,
-  getAlbumTracks,
-};
+import { API_URL } from './config';
+
+export default class SpotifyWrapper {
+  constructor(options) {
+    this.apiURL = options.apiURL || API_URL;
+    this.token = options.token;
+
+    this.album = album.bind(this)();
+    this.search = search.bind(this)();
+  }
+
+  request(url) {
+    const headers = {
+      headers: {
+        Authorization: `Bearer ${this.token}`,
+      },
+    };
+    return fetch(url, headers);
+  }
+}
