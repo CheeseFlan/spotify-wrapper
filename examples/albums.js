c2c3b7f0012a3e56a58228e24551979da5c8c8f6
John Henrique <joaoh3326@gmail.com>
2017-08-30 16:41:22 -0300

    new erro

--- examples/albums.js
+++ examples/albums.js
@@ -1,7 +1,13 @@
+// to run: babel-node album.js
+
 global.fetch = require('node-fetch');
 
-import { searchAlbums } from '../src/search';
+import SpotifyWrapper from '../src/index';
 
-const albums = searchAlbums('Incubus');
+const spotify = new SpotifyWrapper({
+  token: 'BQAnFMB4ZEw_VpAU0e5tM1lCVDRXHmVKo_pBj5AL-eYS8EFVviG5RlZim5QXNtQlh_aeL1UvVFCestz50nzUyLYOWQ_eRI2B36CoNw6c8EuCzPHbQRZnSJ8NMDlxdyVxZUvBJtuULg'
+})
+
+const albums = spotify.search.albums('Incubus');
 
 albums.then(data => console.log(data));
